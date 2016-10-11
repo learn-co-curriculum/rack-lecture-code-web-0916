d0408d627a562cf7bc6075884a1666ebfaa19d27
ipc103 <iancandysemail@gmail.com>
2016-10-11 17:37:55 -0400

    solution code

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+require_all 'app'
+
+require_relative '../application'
+
+ActiveRecord::Base.establish_connection({
+  :adapter => "sqlite3",
+  :database  => "db/database.sqlite3"})
